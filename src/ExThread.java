263b81a71522c24d79d6127902eb8fbc56f7bb23
Claire AVILA <avila.claire@gmail.com>
2024-01-23 15:06:46 +0100

    fin exo 2.1

--- src/ExThread.java
+++ src/ExThread.java
@@ -7,18 +7,23 @@ public class ExThread extends Thread {
 	}
 
 	public void run() {
-		for (int i =0; i <20 ; i++) 
-		System.out.println(this.getName());
+		for (int i =0; i <5 ; i++) {
+			
+			System.out.print(this.getName());
+			
+		}
+			
+		System.out.println();
 	}
 	public static void main (String [] args) {
 		
-		ExThread t = new ExThread ("-1-");
-		ExThread t2 = new ExThread ("5-----");ExThread t3 = new ExThread ("2--");
+		ExThread t = new ExThread ("1-");
+		ExThread t2 = new ExThread ("5-----");
+		ExThread t3 = new ExThread ("2--");
 		ExThread t4 = new ExThread ("3---");
+		ExThread t5 = new ExThread ("4----");
 		
-		t.start();t2.start();t3.start();t4.start();
-		
-		
+		t.start();t2.start();t3.start();t4.start();t5.start();
 		
 		}
 		
